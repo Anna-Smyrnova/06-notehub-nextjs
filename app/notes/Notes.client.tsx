63a3207ef1e4c89dcb5bf8ab364d1c6b48eda34b
Anna Smyrnova <anyto4ka777s@gmail.com>
2026-04-23 14:33:42 +0300

    fixed search box

--- app/notes/Notes.client.tsx
+++ app/notes/Notes.client.tsx
@@ -43,7 +43,7 @@ export default function NotesClient() {
   return (
     <div className={css.app}>
       <header className={css.toolbar}>
-        <SearchBox value={searchQuery} onSearch={handleSearch} />
+        <SearchBox defaultValue={searchQuery} onSearch={handleSearch} />
 
         {isSuccess && totalPages > 1 && (
           <Pagination
